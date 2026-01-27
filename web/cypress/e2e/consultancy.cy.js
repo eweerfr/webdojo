1b61731494e45a8cba97c74814e7bad35c31f06b
eweer <thehatedbrosu@gmail.com>
2026-01-27 11:49:29 -0300

    comandos e ajustes

---
+++ web/cypress/e2e/consultancy.cy.js
@@ -0,0 +1,105 @@
+describe('Formulário de Consultoria', () => {
+    it('Deve solicitar consultoria individual', () => {
+        cy.start()
+        cy.submitLoginForm('papito@webdojo.com', 'katana123')
+
+        cy.goTo('Formulários', 'Consultoria')
+
+        cy.get('input[placeholder="Digite seu nome completo"]')
+            .type('Fernando Papito')
+
+        cy.get('#email')
+            .type('papito@teste.com.br')
+
+        cy.get('input[placeholder="(00) 00000-0000"]')
+            .type('11989899090')
+            .should('have.value', '(11) 98989-9090')
+
+        //localizar como fosse XPath
+        cy.contains('label', 'Tipo de Consultoria')
+            .parent()
+            .find('select')
+            .select('Individual')
+
+        cy.contains('label', 'Pessoa Física')
+            .find('input')
+            .check()
+            .should('be.checked')
+
+        cy.contains('label', 'Pessoa Jurídica')
+            .find('input')
+            .should('be.not.checked')
+
+        cy.contains('label', 'CPF')
+            .parent()
+            .find('input')
+            .type('36991120049')
+            .should('be.value', '369.911.200-49')
+
+
+        const discoveryChannels = [
+            'Instagram',
+            'LinkedIn',
+            'Udemy',
+            'YouTube',
+            'Indicação de Amigo'
+        ]
+
+        discoveryChannels.forEach((channel) => {
+            cy.contains('label', channel)
+                .find('input')
+                .check()
+                .should('be.checked')
+        })
+
+        cy.get('input[type="file"')
+            .selectFile('./cypress/fixtures/document.pdf', { force: true })
+
+        cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
+            .type('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+            .should('be.value', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+
+
+        const techs = [
+            'Cypress',
+            'Playwright',
+            'Selenium',
+            'Robot Framework',
+            'WebDriverIO'
+        ]
+
+        techs.forEach((tech) => {
+            cy.get('input[placeholder="Digite uma tecnologia e pressione Enter"]')
+                .type(tech)
+                .type('{enter}')
+
+            cy.contains('label', 'Tecnologias')
+                .parent()
+                .contains('span', tech)
+                .should('be.visible')
+        })
+
+        cy.contains('label', 'Tecnologias')
+            .parent()
+            .contains('span', 'WebDriverIO') //clica em excluir em uma tag para testes
+            .find('button')
+            .click()
+
+        cy.contains('WebDriverIO')
+            .should('not.exist')        //valida que nao existe mais
+
+
+        cy.contains('label', 'termos de uso')
+            .find('input')
+            .check()
+            .should('be.checked')
+
+        cy.contains('button', 'Enviar formulário')
+            .click()
+
+        cy.contains('Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
+            .should('be.visible')
+
+    })
+})
+
