467ee6e73899a0f4b0989ca8cc1bd54f1649ee55
eweerfr <thehatedbrosu@gmail.com>
2026-02-03 23:23:57 -0300

    add github page testing

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -28,29 +28,15 @@ describe('Formulário de Consultoria', () => {
             { label: 'termos de uso', message: 'Você precisa aceitar os termos de uso' }
         ]
 
-        cy.contains('label', 'Nome Completo')
-            .parent()
-            .find('p')
-            .should('be.visible')
-            .should('have.text', 'Campo obrigatório')
-            .and('have.class', 'text-red-400')
-            .and('have.css', 'color', 'rgb(248, 113, 113)')
-
-        cy.contains('label', 'Email')
-            .parent()
-            .find('p')
-            .should('be.visible')
-            .should('have.text', 'Campo obrigatório')
-            .and('have.class', 'text-red-400')
-            .and('have.css', 'color', 'rgb(248, 113, 113)')
-
-        cy.contains('label', 'termos de uso')
-            .parent()
-            .find('p')
-            .should('be.visible')
-            .should('have.text', 'Você precisa aceitar os termos de uso')
-            .and('have.class', 'text-red-400')
-            .and('have.css', 'color', 'rgb(248, 113, 113)')
+        requiredFields.forEach(({ label, message }) => {
+            cy.contains('label', label)
+                .parent()
+                .find('p')
+                .should('be.visible')
+                .should('have.text', message)
+                .and('have.class', 'text-red-400')
+                .and('have.css', 'color', 'rgb(248, 113, 113)')
+        })
     })
 })
 
---
+++ web/cypress/e2e/github.cy.js
@@ -0,0 +1,58 @@
+describe('Gerenciamento de perfis no github', () => {
+    beforeEach(() => {
+        cy.login()
+        cy.goTo('Tabela', 'Perfis do GitHub')
+    })
+
+    it('Deve poder cadastrar um novo perfil do GitHub', () => {
+        cy.log('todo')
+
+        cy.get('#name').type('Fernando Papito')
+        cy.get('#username').type('qapapito')
+        cy.get('#profile').type('QA')
+
+        cy.contains('button', 'Adicionar Perfil').click()
+
+        cy.get('#name').type('Fernando Papito')
+        cy.get('#username').type('papitodev')
+        cy.get('#profile').type('QA')
+
+        cy.contains('button', 'Adicionar Perfil').click()
+
+        cy.contains('table tbody tr', 'papitodev')
+            .should('be.visible')
+            .as('trProfile')
+
+        cy.get('@trProfile')
+            .contains('Fernando Papito')
+            .should('be.visible')
+
+        cy.get('@trProfile')
+            .contains('QA')
+            .should('be.visible')
+    })
+
+    it.only('Deve poder remover um perfil do github', () => {
+
+        const profile = {
+            name: 'Fernando Papito',
+            username: 'papito123',
+            desc: 'QA'
+        }
+
+        cy.get('#name').type(profile.name)
+        cy.get('#username').type(profile.username)
+        cy.get('#profile').type(profile.desc)
+
+        cy.contains('button', 'Adicionar Perfil').click()
+
+        cy.contains('table tbody tr', profile.username)
+            .should('be.visible')
+            .as('trProfile')
+
+        cy.get('@trProfile').find('button[title="Remover perfil"]').click()
+
+        cy.contains('table tbody', profile.username)
+            .should('not.exist')
+    })
+})
